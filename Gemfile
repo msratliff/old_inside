02fa408c0006ffa48edaa7bffdbe66c1be86f21c
Mark Ratliff <mark.s.ratliff@gmail.com>
2017-08-22 21:02:32 -0400

    Added geocoding to Venue, adjusted venue database column names to accomodate geocoder gem

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'geocoder', '~> 1.4', '>= 1.4.4'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,11 @@
 class Venue < ApplicationRecord
   has_many :users
+
+  geocoded_by :address
+  after_validation :geocode, if: ->(obj){ obj.city.present? and obj.city_changed? }
+
+  def address
+  	[street, city, state, zipcode, 'USA'].compact.join(', ')
+  end
+
 end
